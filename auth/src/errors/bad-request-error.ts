3c9106b75a9035ea875e6a8bc1d8631eaf5b02fd
Yuvaraja Balamurugan <yuv.slm@gmail.com>
2021-02-15 19:12:06 +0100

    Add mongodb and setup user signup

---
+++ auth/src/errors/bad-request-error.ts
@@ -0,0 +1,18 @@
+import CustomError from './custom-error';
+
+export default class BadRequestError extends CustomError {
+  statusCode = 400;
+  
+  constructor(private reason: string) {
+    super(reason);
+
+    // Only because we are extending a built in class.
+    Object.setPrototypeOf(this, BadRequestError.prototype);
+  }
+
+  serializeErrors() {
+    return [
+      { message: this.reason }
+    ];
+  }
+}
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import 'express-async-errors';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
@@ -24,6 +26,22 @@ app.all('*', async () => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log('Listening to port 3000!!!!!');
-});
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-svc:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+  } catch (err) {
+    console.error(err);
+  }
+
+  console.log('Connected to mongoDB');
+  
+  app.listen(3000, () => {
+    console.log('Listening to port 3000!!!!!');
+  });
+}
+
+start();
